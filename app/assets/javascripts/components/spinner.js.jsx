3d69afdb514b8b67ef11a884d4f5abaea8c61dc0
Timothee Guerin <timothee.guerin@outlook.com>
2014-11-20 12:31:43 -0500

    better spinner

--- app/assets/javascripts/components/spinner.js.jsx
+++ app/assets/javascripts/components/spinner.js.jsx
@@ -3,26 +3,7 @@
 var Spinner = React.createClass({
     render: function () {
         return (
-            <div className="spinner spinner-sm">
-                <div className="spinner-container container1">
-                    <div className="circle1"></div>
-                    <div className="circle2"></div>
-                    <div className="circle3"></div>
-                    <div className="circle4"></div>
-                </div>
-                <div className="spinner-container container2">
-                    <div className="circle1"></div>
-                    <div className="circle2"></div>
-                    <div className="circle3"></div>
-                    <div className="circle4"></div>
-                </div>
-                <div className="spinner-container container3">
-                    <div className="circle1"></div>
-                    <div className="circle2"></div>
-                    <div className="circle3"></div>
-                    <div className="circle4"></div>
-                </div>
-            </div>
+            <i className='fa fa-circle-o-notch fa-spin'></i>
         )
     }
 });
